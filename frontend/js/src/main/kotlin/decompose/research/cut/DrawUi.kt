56188d60025e4c502a1d2eb3ee1fb07bfd4bf0ba
max <kellia-16@yandex.ru>
2021-07-26 18:00:57 +0300

    реализовал установку размера для канвасов draw и shapes реализовал апдейт размера канвасов

--- frontend/js/src/main/kotlin/decompose/research/cut/DrawUi.kt
+++ frontend/js/src/main/kotlin/decompose/research/cut/DrawUi.kt
@@ -1,24 +1,33 @@
 package decompose.research.cut
 
+import com.badoo.reaktive.observable.Observable
 import components.draw.Draw
 import components.draw.Draw.Model
+import controller.CutController
 import decompose.RenderableComponent
 import decompose.research.cut.DrawUi.State
 import kotlinx.css.*
 import kotlinx.html.classes
 import kotlinx.html.js.*
-import react.RBuilder
-import react.RState
 import react.dom.attrs
 import styled.css
 import styled.styledCanvas
 import styled.styledDiv
 import decompose.Props
+import kotlinx.browser.window
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.w3c.dom.Element
+import react.*
+import react.dom.findDOMNode
+import research.cut.*
 
 class DrawUi(props: Props<Draw>) : RenderableComponent<Draw, State>(
   props = props,
   initialState = State(model = props.component.model.value)
 ) {
+
+  private var testRef: Element? = null
 
   init {
     component.model.bindToState { model = it }
@@ -34,15 +43,43 @@ class DrawUi(props: Props<Draw>) : RenderableComponent<Draw, State>(
         width = 100.pct
         height = 100.pct
       }
-      styledCanvas {
-        this@styledCanvas.attrs {
-          classes = classes + "draw_canvas_${state.model.cutType}"
-          width = 100.pct.toString()
-          height = 100.pct.toString()
+      ref {
+        testRef = findDOMNode(it)
+        if (testRef != null) {
+          callToRenderContent()
         }
       }
     }
+  }
 
+  override fun componentDidMount() {
+    window.addEventListener(type = "resize", callback = { callToRenderContent() })
+  }
+
+  private fun callToRenderContent() {
+    GlobalScope.launch {
+      testRef?.let {
+        renderContent(it.clientHeight, it.clientWidth)
+      }
+    }
+  }
+
+  private fun renderContent(clientHeight: Int, clientWidth: Int) {
+    testRef?.let {
+      react.dom.render(it) {
+        styledCanvas {
+          this@styledCanvas.attrs {
+            classes = classes + "draw_canvas_${state.model.cutType}"
+            width = clientWidth.toString()
+            height = clientHeight.toString()
+          }
+        }
+      }
+    }
+  }
+
+  override fun componentWillUnmount() {
+    window.removeEventListener(type = "resize", {})
   }
 
   class State(var model: Model) : RState
--- frontend/js/src/main/kotlin/decompose/research/cut/ShapesUi.kt
+++ frontend/js/src/main/kotlin/decompose/research/cut/ShapesUi.kt
@@ -5,11 +5,16 @@ import components.shapes.Shapes.Model
 import decompose.Props
 import decompose.RenderableComponent
 import decompose.research.cut.ShapesUi.State
+import kotlinx.browser.window
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import kotlinx.css.*
 import kotlinx.html.classes
+import org.w3c.dom.Element
 import react.RBuilder
 import react.RState
 import react.dom.attrs
+import react.dom.findDOMNode
 import styled.css
 import styled.styledCanvas
 import styled.styledDiv
@@ -18,6 +23,8 @@ class ShapesUi(props: Props<Shapes>) : RenderableComponent<Shapes, State>(
   props = props,
   initialState = State(model = props.component.model.value)
 ) {
+
+  private var testRef: Element? = null
 
   init {
     component.model.bindToState { model = it }
@@ -33,14 +40,43 @@ class ShapesUi(props: Props<Shapes>) : RenderableComponent<Shapes, State>(
         width = 100.pct
         height = 100.pct
       }
-      styledCanvas {
-        this@styledCanvas.attrs {
-          classes = classes + "shape_canvas_${state.model.cutType}"
-          width = 100.pct.toString()
-          height = 100.pct.toString()
+      ref {
+        testRef = findDOMNode(it)
+        if (testRef != null) {
+          callToRenderContent()
         }
       }
     }
+  }
+
+  override fun componentDidMount() {
+    window.addEventListener(type = "resize", callback = { callToRenderContent() })
+  }
+
+  private fun callToRenderContent() {
+    GlobalScope.launch {
+      testRef?.let {
+        renderContent(it.clientHeight, it.clientWidth)
+      }
+    }
+  }
+
+  private fun renderContent(clientHeight: Int, clientWidth: Int) {
+    testRef?.let {
+      react.dom.render(it) {
+        styledCanvas {
+          this@styledCanvas.attrs {
+            classes = classes + "shape_canvas_${state.model.cutType}"
+            width = clientWidth.toString()
+            height = clientHeight.toString()
+          }
+        }
+      }
+    }
+  }
+
+  override fun componentWillUnmount() {
+    window.removeEventListener(type = "resize", {})
   }
 
   class State(var model: Model) : RState
